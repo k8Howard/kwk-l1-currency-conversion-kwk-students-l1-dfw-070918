6412f1d238c2c663c694824bb3558beed00d8a08
k8Howard <sweetk8@me.com>
2018-07-11 14:17:32 +0000

    automatically backed up by learn

--- currency.rb
+++ currency.rb
@@ -34,7 +34,27 @@ end
 
 
 
-choice = gets.chomp(). to_i
+choice = gets.chomp().to_i
 
 case choice 
 when 1
+  usd_to_eur(amount)
+when 2
+  usd_to_jpy
+when 3
+usd_to_gbp
+when 4
+  usd_to_aud
+when 5
+  eur_to_usd
+when 6
+  jpy_to_usd
+when 7 
+  gbp_to_usd
+when 8 
+  aud_to_usd
+  
+  
+  
+  
+  
