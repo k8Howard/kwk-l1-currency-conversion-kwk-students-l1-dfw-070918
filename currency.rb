b66bd34e480e0271ed03ccd9fd9a1dff3d302a4f
k8Howard <sweetk8@me.com>
2018-07-11 14:07:17 +0000

    automatically backed up by learn

---
+++ currency.rb
@@ -0,0 +1,19 @@
+def usd_to_eur(amount)
+  new_currency = amount * 0.86
+end
+
+def eur_to_usd(amount)
+  new_currency = amount * 1.16
+end
+
+def usd_to_jpy(amount)
+  new_currency = amount * 110.90
+end
+
+def jpy_to_usd(amount)
+  new_currency = amount * 0.0090
+end
+
+def usd_to_gbp(amount)
+  new_currency = amount * 0.76
+end
